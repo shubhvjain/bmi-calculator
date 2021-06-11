27614e9840a62c2e37039aac05cc87cb9aea62b9
Shubh V Jain <shubhvjain@gmail.com>
2021-06-11 14:54:14 +0530

    added more testes

--- test/test.js
+++ test/test.js
@@ -43,15 +43,34 @@ describe("Input data validation for BMI calculation", function () {
 describe("BMI calculation correctness", function () {
   let data = { "HeightCm": 175, "WeightKg": 75 }
   let expData = {
-    "HeightCm": 175, 
+    "HeightCm": 175,
     "WeightKg": 75,
-    "BMI":24.49,
-    "BMICategory":"Normal weight",
-    "HealthRisk":"Low risk"
+    "BMI": 24.49,
+    "BMICategory": "Normal weight",
+    "HealthRisk": "Low risk"
   }
   let bmiData = bmi.computeBMI(data)
-  console.log(bmiData)
-  it("Correct BMI value", function () {assert.strictEqual(bmiData.BMI,expData.BMI)})
-  it("Correct BMI Category", function () {assert.strictEqual(bmiData.BMICategory,expData.BMICategory)})
-  it("Correct Health risk indicator", function () {assert.strictEqual(bmiData.HealthRisk,expData.HealthRisk)})
+  it("Correct BMI value", function () { assert.strictEqual(bmiData.BMI, expData.BMI) })
+  it("Correct BMI Category", function () { assert.strictEqual(bmiData.BMICategory, expData.BMICategory) })
+  it("Correct Health risk indicator", function () { assert.strictEqual(bmiData.HealthRisk, expData.HealthRisk) })
+})
+
+describe("BMI analysis calculation correctness", function () {
+  let bmiValues = [
+    { bmi: 18.49, expected: { cat: "Underweight", risk: "Malnutrition" } },
+    { bmi: 18.5, expected: { cat: "Normal weight", risk: "Low" } },
+    { bmi: 24.99, expected: { cat: "Normal weight", risk: "Low" } },
+    { bmi: 25, expected: { cat: "Overweight", risk: "Enhanced" } },
+    { bmi: 29.99, expected: { cat: "Overweight", risk: "Enhanced" } },
+    { bmi: 30.1, expected: { cat: "Moderately obese", risk: "Medium" } },
+    { bmi: 34.99, expected: { cat: "Moderately obese", risk: "Medium" } },
+    { bmi: 35, expected: {  cat: "Severely obese", risk: "High" } },
+    { bmi: 39.99, expected: {  cat: "Severely obese", risk: "High" } },
+    { bmi: 40, expected: {  cat: "Very severely obese", risk: "Very high"} },
+    { bmi: 400, expected: {  cat: "Very severely obese", risk: "Very high" } },
+  ]
+  bmiValues.forEach(item => {
+    let bmiAnalysis = bmi.analyseBMI(item.bmi)
+    it(`Correct BMI Category and health risk indicator for BMI = ${item.bmi} `, function () { assert.deepStrictEqual(bmiAnalysis, item.expected) })
+  })
 })
